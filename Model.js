a5a7aed7ae0a2b9de6fd5631417cd3193991be92
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2021-04-13 08:57:11 -0300

    All method

--- Model.js
+++ Model.js
@@ -1,10 +1,20 @@
 class Model {
-    constructor(){
+    constructor(records = []){
         this._collection = []
+
+        if(records.length){
+            this.add(records)
+        }
     }
 
-    add(){}
-    all(){}
+    add(records){
+        this._collection.push(...records)
+    }
+
+    all(){
+        return this._collection.map(record => Object.assign({}, record))
+    }
+
     get(){}
     remove(){}
     update(){}
--- Model.spec.js
+++ Model.spec.js
@@ -16,6 +16,61 @@ describe('Model', () => {
             update: expect.any(Function),
         }))
     })
+
+    describe('all', () => {
+        let model
+
+        beforeEach(() => {
+            model = new Model
+        })
+
+        describe('When there is data recorded', () => {
+            test('should return the stored data', () => {
+                const heroes = [{name: 'Jacob'}, {name: 'Rebeca'}]
+                model.add(heroes)
+                expect(model.all()).toEqual(heroes)
+            });
+
+            describe('When the data is changed', () => {
+                test('should not change the original data', () => {
+                    model.add([{name: 'Moises'}])
+                    const data = model.all()
+
+                    data[0].name = 'Jacob'
+
+                    expect(model._collection[0].name).toBe('Moises')
+                });
+                
+            });
+        });
+
+        describe('When ther is no data recorded', () => {
+            test('should return an empty array', () => {
+                expect(model.all()).toEqual([])
+            });
+        });
+        
+    });
+
+    describe('add', () => {
+        const heroes = ['Moises', 'Abraham']
+        test('should add data to the collection', () => {
+
+            const model = new Model
+            model.add(heroes)
+
+            expect(model._collection).toEqual(heroes)
+        });
+
+        test('should gets called when data is passed to the constructor', () => {
+            const spy = jest.spyOn(Model.prototype, 'add')
+            const model = new Model(heroes)
+
+            expect(spy).toBeCalled()
+
+            spy.mockRestore()
+        });
+    });
 })
 
 
